2f8896a83ad989893b977afadc30cdb89ba92b0d
Michael Haase <darkstar@gernox.de>
2019-06-04 17:51:30 +0200

    wip

--- vag-app/src/app/services/routing/routing.service.ts
+++ vag-app/src/app/services/routing/routing.service.ts
@@ -1,30 +1,28 @@
 import { Injectable } from '@angular/core';
 
-
 declare const google: any;
 
-@Injectable({
-  providedIn: 'root',
-})
+@Injectable( {
+    providedIn: 'root',
+} )
 export class RoutingService {
 
     private directionsService: any;
 
-  constructor() {
-      this.directionsService = new google.maps.DirectionsService;
-   }
+    constructor() {
+        this.directionsService = new google.maps.DirectionsService();
+    }
 
-   public navigate() {
-        return this.directionsService.route({
-            origin: {lat:49.450520, lng: 11.080480},
-            destination: {lat: 49.443056, lng: 11.094571},
+    public navigate() {
+        return this.directionsService.route( {
+            origin: { lat: 49.450520, lng: 11.080480 },
+            destination: { lat: 49.443056, lng: 11.094571 },
             travelMode: 'TRANSIT'
-        }, function(response, status) {
-            console.log(response);
-            console.log(status);
-        })
-   }
+        }, function ( response, status ) {
+            console.log( response );
+            console.log( status );
+        } )
+    }
 
-  
-  
+
 }
