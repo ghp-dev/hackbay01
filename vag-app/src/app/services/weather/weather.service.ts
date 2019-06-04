5c1416141478c684aa3af6e8d508690c326847c5
Michael Kosny <michael.kosny@datev.de>
2019-06-04 17:22:21 +0200

    Entity vorerst entfernt

--- vag-app/src/app/services/weather/weather.service.ts
+++ vag-app/src/app/services/weather/weather.service.ts
@@ -1,8 +1,8 @@
 import {HttpClient} from '@angular/common/http';
 import {Injectable} from '@angular/core';
 import {Observable} from 'rxjs';
-import {WeatherForecastHourly} from '../../shared/weather-forecast-hourly.entity';
 import { EndpointsConfig } from 'src/app/app.endpoints.config';
+import { map, catchError, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -12,7 +12,12 @@ export class WeatherService {
   constructor(private endpoints: EndpointsConfig, private httpClient: HttpClient) {
   }
 
-  fetchWeatherForecastHourly(): Observable<WeatherForecastHourly> {
-    return this.httpClient.get<WeatherForecastHourly>(this.endpoints.fetchWeatherForecastHourly());
+  fetchWeatherForecastHourly(): Observable<any> {
+    return this.httpClient.get(this.endpoints.fetchWeatherForecastHourly()).pipe(
+      map(this.extractData));
+  }
+
+  private extractData(res: Response) {
+    return res || { };
   }
 }
