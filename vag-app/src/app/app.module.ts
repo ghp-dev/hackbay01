7b4dfb35654918fc3e29ece469112767807ccb42
Maximilian Schönberger <max@MaxiBook-Datev.local>
2019-06-04 16:15:05 +0200

    timeline package eingebaut

--- vag-app/src/app/app.module.ts
+++ vag-app/src/app/app.module.ts
@@ -2,15 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MglTimelineModule } from 'angular-mgl-timeline';
 import { AppComponent } from './app.component';
+import { TimelineComponent } from './timeline/timeline.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TimelineComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MglTimelineModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ vag-app/src/app/timeline/timeline.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-timeline',
+  templateUrl: './timeline.component.html',
+  styleUrls: ['./timeline.component.sass']
+})
+export class TimelineComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
