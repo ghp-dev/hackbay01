717d6430fe49881d9c6988e69804bd3bb41b5d16
Maximilian Schönberger <max@MaxiBook-Datev.local>
2019-06-04 16:39:11 +0200

    sass zu scss geändert

--- vag-app/src/app/timeline/timeline.component.ts
+++ vag-app/src/app/timeline/timeline.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-timeline',
   templateUrl: './timeline.component.html',
-  styleUrls: ['./timeline.component.sass']
+  styleUrls: ['./timeline.component.scss']
 })
 export class TimelineComponent implements OnInit {
 
