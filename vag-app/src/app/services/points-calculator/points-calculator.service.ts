c3dcacaf0cb66ba5a9b50029d57ca3f0cb2d4747
Michael Kosny <michael.kosny@datev.de>
2019-06-05 10:31:27 +0200

    Am Calculator gearbeitet

--- vag-app/src/app/services/points-calculator/points-calculator.service.ts
+++ vag-app/src/app/services/points-calculator/points-calculator.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Weather } from 'src/app/shared/weather.entity';
 import { RoutingInfo } from 'src/app/shared/routing-info.entity';
+import { CapacityRed, CapacityYellow, CapacityGreen } from 'src/app/services/vag-capacity/capacity-state';
 
 @Injectable({
   providedIn: 'root',
@@ -12,11 +13,31 @@ export class PointsCalculatorService {
 
   calculatePoints(weatherData: Weather, routeData: RoutingInfo, capacityData: number): number {
     console.log('Points-Calculator: Init-State');
+    let points = 0;
     // Info: Wetterdaten nur für Berechnung der Punkte für Autofahrten relevant:
     //        (je wärmer, desto mehr muss das Auto kühlen, desto mehr Kraftstoff wird verbraucht)
 
-    // TODO: Berechnungslogik implementieren
+    // Points für capacityData
+    switch (capacityData) {
+      case CapacityGreen:
+        points += 80;
+        break;
 
-    return 123;
+      case CapacityYellow:
+        points += 40;
+        break;
+
+      case CapacityRed:
+        points += 10;
+        break;
+
+      default:
+        points += 100;
+    }
+
+    // Points für Routen-Schritte-Verkehrsmittel
+
+
+    return points;
   }
 }
