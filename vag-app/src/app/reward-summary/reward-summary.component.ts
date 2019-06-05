63f775dfee2bc9eae0f993259ae22699afbcf536
ghp-dev <ghp-dev@gmx.de>
2019-06-05 11:36:18 +0200

    Reward Summary Screen Fake

---
+++ vag-app/src/app/reward-summary/reward-summary.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-reward-summary',
+  templateUrl: './reward-summary.component.html',
+  styleUrls: ['./reward-summary.component.scss']
+})
+export class RewardSummaryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
